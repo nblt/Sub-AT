8bc0ed96b74543d311f8c495898f13eb6bb6b5a2
nblt <ltwtzjh@126.com>
2021-11-27 20:55:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,10 +18,10 @@ do
 
     #-------------------------------------------------------------#
     # multi-step PGD AT
-    # CUDA_VISIBLE_DEVICES=$device python -u train_adv.py --autoattack --randomseed $seed --datasets $datasets --lr 0.1 --train_eps $eps --test_eps $eps  --wd 0.0005 --arch=$model --epochs=200  --save-dir=pgd10_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a pgd10_seed$seed\_log_$model\_$datasets\_$eps
+    # CUDA_VISIBLE_DEVICES=$device python -u train_adv.py --randomseed $seed --datasets $datasets --lr 0.1 --train_eps $eps --test_eps $eps  --wd 0.0005 --arch=$model --epochs=200  --save-dir=pgd10_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a pgd10_seed$seed\_log_$model\_$datasets\_$eps
     
     # multi-step PGD Sub-AT
-    # CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py --autoattack  --datasets $datasets --lr 1  --train_eps $eps --test_eps $eps  --params_start 0 --params_end 200  --batch-size 128  --n_components 120 --arch=$model --epochs=40  --save-dir=pgd10_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a pgd10_seed$seed\_log_$model\_$datasets\_$eps\_psgd
+    # CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py  --datasets $datasets --lr 1  --train_eps $eps --test_eps $eps  --params_start 0 --params_end 200  --batch-size 128  --n_components 120 --arch=$model --epochs=40  --save-dir=pgd10_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a pgd10_seed$seed\_log_$model\_$datasets\_$eps\_psgd
     
     # single-step fast Sub-AT (with a larger training radius)
     # CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py  --datasets $datasets --lr 1  --train_eps 12 --test_eps $eps --train_step 1 --test_step 20 --train_gamma 15 --test_gamma 2  --params_start 0 --params_end 131  --batch-size 128  --n_components 80 --arch=$model --epochs=40  --save-dir=pgd10_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a fastfgsm_seed$seed\_log_$model\_$datasets\_$eps\_psgd
