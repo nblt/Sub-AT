5fdd51b84113d7b23e7b120c486f72c8b0bb0903
nblt <ltwtzjh@126.com>
2021-11-27 10:47:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,14 @@ device=0
 
 for model in PreActResNet18
 do
-
+    
+    # single-step fast AT
     CUDA_VISIBLE_DEVICES=$device python -u train_adv.py --randomseed $seed  --datasets $datasets --lr 0.1 --train_eps $eps --test_eps $eps --train_step 1 --test_step 20 --train_gamma 10 --test_gamma 2 --wd 0.0005 --arch=$model --epochs=200  --save-dir=fastfgsm_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a new_seed$seed\_fastfgsm_log_$model\_$datasets\_$eps
+    
+    # single-step fast Sub-AT
     CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py  --datasets $datasets --lr 1  --train_eps 8 --test_eps $eps --train_step 1 --test_step 20 --train_gamma 10 --test_gamma 2  --params_start 0 --params_end 131  --batch-size 128  --n_components 80 --arch=$model --epochs=40  --save-dir=fastfgsm_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a fastfgsm_seed$seed\_log_$model\_$datasets\_$eps\_psgd
+    
+    # single-step fast Sub-AT (with a larger training radius)
     CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py  --datasets $datasets --lr 1  --train_eps 12 --test_eps $eps --train_step 1 --test_step 20 --train_gamma 15 --test_gamma 2  --params_start 0 --params_end 131  --batch-size 128  --n_components 80 --arch=$model --epochs=40  --save-dir=fastfgsm_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a fastfgsm_seed$seed\_log_$model\_$datasets\_$eps\_psgd
     CUDA_VISIBLE_DEVICES=$device python -u train_adv_psgd.py  --datasets $datasets --lr 1  --train_eps 16 --test_eps $eps --train_step 1 --test_step 20 --train_gamma 20 --test_gamma 2  --params_start 0 --params_end 131  --batch-size 128  --n_components 80 --arch=$model --epochs=40  --save-dir=fastfgsm_save_seed$seed\_$model\_$datasets\_eps$eps |& tee -a fastfgsm_seed$seed\_log_$model\_$datasets\_$eps\_psgd
 
